88d718cb4cfddc0f50901f9c244b353ea01a4885
¨WillyBarrios¨ <romeob-@outlook.com>
2025-07-31 22:02:02 -0600

    Cambie a rutas estaticas con HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthContext } from './context/AuthContext';
 import { useContext } from 'react';
 import Home from './pages/Home';
